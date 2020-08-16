2c5f428e5898373587aa866cdac068d506a28d6d
keerthanacse13@gmail.com <keerthanacse13@gmail.com>
2020-08-16 21:01:06 +0530

    Redis impl initial setup

---
+++ src/main/java/com/cache/redisImpl/RedisImplApplication.java
@@ -0,0 +1,13 @@
+package com.cache.redisImpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisImplApplication.class, args);
+	}
+
+}
