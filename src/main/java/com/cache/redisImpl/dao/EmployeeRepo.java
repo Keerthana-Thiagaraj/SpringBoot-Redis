e5752af9dab982415a41b59e43ddc3474e0ec033
keerthanacse13@gmail.com <keerthanacse13@gmail.com>
2020-08-17 18:20:15 +0530

    dao added for list

--- src/main/java/com/cache/redisImpl/dao/EmployeeRepo.java
+++ src/main/java/com/cache/redisImpl/dao/EmployeeRepo.java
@@ -1,12 +1,22 @@
 package com.cache.redisImpl.dao;
 
+import com.cache.redisImpl.model.Employee;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmployeeRepo {
 
+    //String
     void setEmployeeAsString(String id, String employee);
 
     String getEmployee(String id);
+
+    //List
+
+    void addEmployeeList(Employee employee);
+
+    List<Employee> getEmployeeList();
 
 }
--- src/main/java/com/cache/redisImpl/dao/EmployeeRepoImpl.java
+++ src/main/java/com/cache/redisImpl/dao/EmployeeRepoImpl.java
@@ -1,16 +1,28 @@
 package com.cache.redisImpl.dao;
 
+import com.cache.redisImpl.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Repository
 public class EmployeeRepoImpl implements EmployeeRepo {
 
+    public static final String REDIS_LIST_KEY = "EmployeeList";
+
     @Autowired
     private RedisTemplate<String, Object> redisTemplate; //Access Redis database, by using bean defined in config class
+
+    @Autowired
+    @Qualifier("listOperations")
+    private ListOperations<String, Employee> listOperations;
+
+    //String operations
 
     @Override
     public void setEmployeeAsString(String id, String employee) {
@@ -21,5 +33,17 @@ public class EmployeeRepoImpl implements EmployeeRepo {
     @Override
     public String getEmployee(String id) {
         return (String) redisTemplate.opsForValue().get(id);
+    }
+
+    //List operations
+    @Override
+    public void addEmployeeList(Employee employee) {
+//        redisTemplate.opsForList().leftPush(REDIS_LIST_KEY, employee);
+        listOperations.leftPush(REDIS_LIST_KEY, employee); //opsforlist is defined as bean in config
+    }
+
+    @Override
+    public List<Employee> getEmployeeList() {
+        return listOperations.range(REDIS_LIST_KEY, 0, -1);
     }
 }
