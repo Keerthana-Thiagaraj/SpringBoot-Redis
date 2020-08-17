ae937aec55ccf0a3f4f3cb9599e59f2f64cc4173
keerthanacse13@gmail.com <keerthanacse13@gmail.com>
2020-08-17 17:26:02 +0530

    serializer added in config

--- src/main/java/com/cache/redisImpl/config/redisConfig.java
+++ src/main/java/com/cache/redisImpl/config/redisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class redisConfig {
@@ -20,6 +21,7 @@ public class redisConfig {
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer()); // added for knowing the id value in the cli console
         return template;
     }
 }
