428f5b1fc94017a8d1794bae02878636964fd0af
keerthanacse13@gmail.com <keerthanacse13@gmail.com>
2020-08-17 18:21:07 +0530

    endpoints added for list ops

--- src/main/java/com/cache/redisImpl/controller/EmployeeController.java
+++ src/main/java/com/cache/redisImpl/controller/EmployeeController.java
@@ -7,12 +7,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class EmployeeController {
 
     @Autowired
     EmployeeService employeeService;
 
+    //String end points
     @PostMapping("/employee/add")
     public void addEmployee(@RequestBody Employee employee) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
@@ -22,5 +25,17 @@ public class EmployeeController {
     @GetMapping("/employee/{id}")
     public String getEmployee(@PathVariable String id) {
         return employeeService.getEmployee(id);
+    }
+
+    //List endpoints
+
+    @PostMapping("/employee/addList")
+    public void addEmployeeList(@RequestBody Employee employee) {
+        employeeService.addEmployeeList(employee);
+    }
+
+    @GetMapping("/employee/getEmployeeList")
+    public List<Employee> getEmployeeList() {
+        return employeeService.getEmployeeList();
     }
 }
